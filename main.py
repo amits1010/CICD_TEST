a1094f5cc9d91b25c34f660351cec8901ec29307
amitrhibhus <amit.singh@rhibhus.com>
2023-09-28 11:16:51 +0530

    first code

---
+++ main.py
@@ -0,0 +1,14 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+	return 'Hello World'
+
+# main driver function
+if __name__ == '__main__':
+	app.run()
